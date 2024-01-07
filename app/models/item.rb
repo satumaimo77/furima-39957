73124e6ec67c49b38ae99a8d1a668af669006212
satumaimo77 <lucky.no0077@gmail.com>
2024-01-07 17:11:49 +0900

    画像の保存、編集ファイルの読み込み

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
   validates :name, presence: true
   validates :info, presence: true
   validates :price, presence: true
+  validates :image, presence: true
 end
