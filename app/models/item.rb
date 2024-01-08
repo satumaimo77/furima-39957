12c42ee843d87682e083fe0f6db0a4a9a07b7b30
satumaimo77 <lucky.no0077@gmail.com>
2024-01-08 18:48:42 +0900

    activehash category_idの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 
   validates :name, presence: true
   validates :info, presence: true
   validates :price, presence: true
   validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
 end
