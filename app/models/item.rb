c7e090f5d31ace43beff755f9f6024cfe0a7f363
satumaimo77 <lucky.no0077@gmail.com>
2024-01-08 19:20:09 +0900

    activehash 配送料について

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,8 @@ class Item < ApplicationRecord
   belongs_to :category
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :sales_status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_fee_status
 
   validates :name, presence: true
   validates :info, presence: true
@@ -12,4 +14,5 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank"}
 end
