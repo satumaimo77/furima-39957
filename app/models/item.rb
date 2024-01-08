891feb7619f881d7eb1d685cdac8c47e7c26d54a
satumaimo77 <lucky.no0077@gmail.com>
2024-01-08 19:12:43 +0900

    activehash 商品の状態の確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :sales_status
 
   validates :name, presence: true
   validates :info, presence: true
   validates :price, presence: true
   validates :image, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank"}
 end
