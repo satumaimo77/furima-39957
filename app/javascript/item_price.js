8552aff93eab510541e351dad44f8bce2df2ec43
satumaimo77 <lucky.no0077@gmail.com>
2024-01-11 19:08:06 +0900

    利益などの表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+function itemPrice (){
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const tax = Math.floor(inputValue * 0.1)
+    const profit = Number(inputValue) + Number(tax)
+    addTaxDom.innerHTML = tax
+    profitDom.innerHTML = profit
+  })
+ };
+ 
+ window.addEventListener('turbo:load', itemPrice);
