5bd18df2d0e9af520b206a56563147a1108c5c59
satumaimo77 <lucky.no0077@gmail.com>
2024-01-11 19:48:54 +0900

    fakerの編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     prefecture_id            { Faker::Number.between(from: 2, to: 48) }
     scheduled_delivery_id    { Faker::Number.between(from: 2, to: 4) }
     price                    { Faker::Number.between(from: 300, to: 9999999) }
-
+    association :user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
