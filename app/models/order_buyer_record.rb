3df7913537cf772ef9475a4806ded490d469ea54
satumaimo77 <lucky.no0077@gmail.com>
2024-01-23 20:25:46 +0900

    バリデーションの変更

--- app/models/order_buyer_record.rb
+++ app/models/order_buyer_record.rb
@@ -8,7 +8,7 @@ class OrderBuyerRecord
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :addresses
-    validates :phone_number, numericality: {only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid'}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: 'is invalid'}
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
