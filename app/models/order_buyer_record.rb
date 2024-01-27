ca9e73203b61d135759f7edbb20076a4a34d3cef
satumaimo77 <lucky.no0077@gmail.com>
2024-01-27 12:05:12 +0900

    トークンのモデルでの受け渡しの実装

--- app/models/order_buyer_record.rb
+++ app/models/order_buyer_record.rb
@@ -1,10 +1,11 @@
 class OrderBuyerRecord
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :addresses
@@ -13,7 +14,7 @@ class OrderBuyerRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    order = Order.create(user_id: user_id,item_id: item_id)
+    order = Order.create(user_id: user_id,item_id: item_id,token: token)
     BuyerRecord.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
